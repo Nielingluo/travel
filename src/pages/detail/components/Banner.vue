24e9be25b223b76af2bca65293a7d9193605df08
Nielingluo <nielingluo@gmail.com>
2018-05-28 16:03:05 +0800

    'banner_finish'

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+      <div class="banner" @click="handleBannerClick">
+        <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1710/7f/7f06f56b02987c66a3.img.jpg_600x330_029cf445.jpg" alt="">
+        <div class="banner-info">
+          <div class="banner-title">库木塔格沙漠公园</div>
+          <div class="banner-number"><span class="iconfont banner-icon">&#xe604;</span>8</div>
+        </div>
+      </div>
+      <Common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></Common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1710/7f/7f06f56b02987c66a3.img.jpg_r_800x800_eb8da27f.jpg', 'http://img1.qunarzz.com/sight/p0/1710/7f/7f06f56b02987c66a3.img.jpg_r_800x800_eb8da27f.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+      position relative
+      overflow hidden
+      height: 0
+      padding-bottom 55%
+      .banner-img
+        width 100%
+      .banner-info
+        display: flex
+        position absolute
+        left:0
+        right 0
+        bottom 0
+        line-height .6rem
+        color #fff
+        background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.7))
+        .banner-title
+          flex 1
+          font-size .32rem
+          padding 0 .2rem
+        .banner-number
+          height:.32rem
+          line-height .32rem
+          margin-top .14rem
+          padding 0 .4rem
+          border-radius .2rem
+          background rgba(0,0,0,.8)
+          font-size .24rem
+          .banner-icon
+            font-size .24rem
+            margin-right .1rem
+</style>
